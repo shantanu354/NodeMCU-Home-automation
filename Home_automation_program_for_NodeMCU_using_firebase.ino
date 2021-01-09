a62555968eb96e3edbdbbefd892d42937147da71
Shantanu Patil <37730225+shantanupatil23@users.noreply.github.com>
2021-01-10 01:28:28 +0530

    Update Home_automation_program_for_NodeMCU_using_firebase.ino

--- Home_automation_program_for_NodeMCU_using_firebase.ino
+++ Home_automation_program_for_NodeMCU_using_firebase.ino
@@ -9,10 +9,10 @@
 #include <FirebaseArduino.h>  
   
 // Set these to run example.  
-#define FIREBASE_HOST "home-automation-401.firebaseio.com"  
-#define FIREBASE_AUTH "R5TFNyTOP6xASytBVAsmujVNsUOteRpEugfgnfSV"  
-#define WIFI_SSID "Anil Niwas"  
-#define WIFI_PASSWORD "gitah3Giaghailie"  
+#define FIREBASE_HOST "example.firebaseio.com"  # Replace with actial address
+#define FIREBASE_AUTH "EXPAMPLE****"  # Replace with actal value  
+#define WIFI_SSID "SSID"    # Replace with actal value  
+#define WIFI_PASSWORD "PASS"    # Replace with actal value  
 
 
 int pin1 = 9; // Sets the value for Pin1 of Relay
@@ -97,124 +97,3 @@ void loop() {
   }
   readFirebase();
 }  
-
-
-
-/*
-#include <Firebase.h>  
-#include <FirebaseArduino.h>  
-#include <FirebaseCloudMessaging.h>  
-#include <FirebaseError.h>  
-#include <FirebaseHttpClient.h>  
-#include <FirebaseObject.h>  
-  
-//  
-// Copyright 2015 Google Inc.  
-//  
-// Licensed under the Apache License, Version 2.0 (the "License");  
-// you may not use this file except in compliance with the License.  
-// You may obtain a copy of the License at  
-//  
-//     http://www.apache.org/licenses/LICENSE-2.0  
-//  
-// Unless required by applicable law or agreed to in writing, software  
-// distributed under the License is distributed on an "AS IS" BASIS,  
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  
-// See the License for the specific language governing permissions and  
-// limitations under the License.  
-//  
-  
-// FirebaseDemo_ESP8266 is a sample that demo the different functions  
-// of the FirebaseArduino API.  
-  
-#include <ESP8266WiFi.h>  
-#include <FirebaseArduino.h>  
-  
-// Set these to run example.  
-#define FIREBASE_HOST "nodemcu-demo-697d8.firebaseio.com"  
-#define FIREBASE_AUTH "YOUR_FIREBASE_AUTH"  
-#define WIFI_SSID "NETGEAR64"  
-#define WIFI_PASSWORD "*JAVATPOINT#"  
-  
-void setup() {  
-  Serial.begin(9600);  
-  
-  // connect to wifi.  
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);  
-  Serial.print("connecting");  
-  while (WiFi.status() != WL_CONNECTED) {  
-    Serial.print(".");  
-    delay(500);  
-  }  
-  Serial.println();  
-  Serial.print("connected: ");  
-  Serial.println(WiFi.localIP());  
-    
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);  
-}  
-  
-int n = 0;  
-  
-void loop() {  
-  // set value  
-  Firebase.setFloat("number", 42.0);  
-  // handle error  
-  if (Firebase.failed()) {  
-      Serial.print("setting /number failed:");  
-      Serial.println(Firebase.error());    
-      return;  
-  }  
-  delay(1000);  
-    
-  // update value  
-  Firebase.setFloat("number", 43.0);  
-  // handle error  
-  if (Firebase.failed()) {  
-      Serial.print("setting /number failed:");  
-      Serial.println(Firebase.error());    
-      return;  
-  }  
-  delay(1000);  
-  
-  // get value   
-  Serial.print("number: ");  
-  Serial.println(Firebase.getFloat("number"));  
-  delay(1000);  
-  
-  // remove value  
-  Firebase.remove("number");  
-  delay(1000);  
-  
-  // set string value  
-  Firebase.setString("message", "hello world");  
-  // handle error  
-  if (Firebase.failed()) {  
-      Serial.print("setting /message failed:");  
-      Serial.println(Firebase.error());    
-      return;  
-  }  
-  delay(1000);  
-    
-  // set bool value  
-  Firebase.setBool("truth", false);  
-  // handle error  
-  if (Firebase.failed()) {  
-      Serial.print("setting /truth failed:");  
-      Serial.println(Firebase.error());    
-      return;  
-  }  
-  delay(1000);  
-  
-  // append a new value to /logs  
-  String name = Firebase.pushInt("logs", n++);  
-  // handle error  
-  if (Firebase.failed()) {  
-      Serial.print("pushing /logs failed:");  
-      Serial.println(Firebase.error());    
-      return;  
-  }  
-  Serial.print("pushed: /logs/");  
-  Serial.println(name);  
-  delay(1000);  
-}  
-*/
